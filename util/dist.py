45553b56759cddca16abb476cc69b3b54e3d4d48
SON <22521260@gm.uit.edu.vn>
2024-10-23 23:23:05 +0700

    test_mode_distributed

--- util/dist.py
+++ util/dist.py
@@ -199,9 +199,43 @@ def save_on_master(*args, **kwargs):
 
 
 def init_distributed_mode(args):
-    print("Not using distributed mode")
-    args.distributed = False
-    return
+    """Initialize distributed training, if appropriate"""
+    if "RANK" in os.environ and "WORLD_SIZE" in os.environ:
+        args.rank = int(os.environ["RANK"])
+        args.world_size = int(os.environ["WORLD_SIZE"])
+        args.gpu = int(os.environ["LOCAL_RANK"])
+    elif "SLURM_PROCID" in os.environ:
+        args.rank = int(os.environ["SLURM_PROCID"])
+        args.gpu = args.rank % torch.cuda.device_count()
+        # CLUSTER SPECIFIC
+        args.world_size = int(os.environ["SLURM_NTASKS"])
+        hostnames = hostlist.expand_hostlist(os.environ["SLURM_JOB_NODELIST"])
+        gpu_ids = os.environ["SLURM_STEP_GPUS"].split(",")
+        os.environ["MASTER_ADDR"] = hostnames[0]
+        os.environ["MASTER_PORT"] = str(
+            12345 + int(min(gpu_ids))
+        )  # to avoid port conflict on the same node
+    else:
+        print("Not using distributed mode")
+        args.distributed = False
+        return
+
+    args.distributed = True
+
+    torch.cuda.set_device(args.gpu)
+    args.dist_backend = "nccl"
+    print(
+        "| distributed init (rank {}): {}".format(args.rank, args.dist_url), flush=True
+    )
+
+    dist.init_process_group(
+        backend=args.dist_backend,
+        init_method=args.dist_url,
+        world_size=args.world_size,
+        rank=args.rank,
+    )
+    dist.barrier()
+    setup_for_distributed(args.rank == 0)
 
 class GatherLayer(torch.autograd.Function):
     """
